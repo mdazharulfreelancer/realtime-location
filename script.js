8a7ad7efb2e15e100e67ddf43ec5eed52c9ccc04
MD Azharul <150342131+mdazharulfreelancer@users.noreply.github.com>
2024-08-21 22:27:53 +0600

    Update script.js

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ navigator.geolocation.watchPosition((position)=>{
     console.log(error)
 },{
     enableHighAccuracy :true,
-     timeout: 5000,
+     timeout: 200,
      maximumAge : 0
 }
 
 )
 
 }
-const map =  L.map("map").setView([0,0], 16)
+const map =  L.map("map").setView([0,0], 10)
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
     attribution : "Sheryians Coding School"
